a9fdea9fba66fba112bd4944b98b609a85bace33
Saleh BUD <saleh.7734@gmail.com>
2022-03-16 09:56:38 +0330

    Lesson 2 / Done

--- Lesson2-2.sol
+++ Lesson2-2.sol
@@ -21,15 +21,38 @@ contract KittyInterface {
 }
 
 contract ZombieFeeding is ZombieFactory {
-    function feedAndMultiply(uint256 _zombieId, uint256 _targetDna) public {
-        address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
-        // Initialize kittyContract here using `ckAddress` from above
-        KittyInterface kittyContract = KittyInterface(ckAddress);
+    address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
+    // Initialize kittyContract here using `ckAddress` from above
+    KittyInterface kittyContract = KittyInterface(ckAddress);
 
+    function feedAndMultiply(
+        uint256 _zombieId,
+        uint256 _targetDna,
+        string memory _species
+    ) public {
         require(msg.sender == zombieToOwner[_zombieId]);
         Zombie storage myZombie = zombies[_zombieId];
         _targetDna = _targetDna % dnaModulus;
         uint256 newDna = (myZombie.dna + _targetDna) / 2;
+        if (
+            keccak256(abi.encodePacked(_species)) ==
+            keccak256(abi.encodePacked("kitty"))
+        ) {
+            newDna = newDna - (newDna % 100) + 99;
+        }
         _createZombie("NoName", newDna);
+    }
+
+    /*
+    The function should then call the kittyContract.
+    getKitty function with _kittyId and store genes in kittyDna.
+    Remember — getKitty returns a ton of variables.
+    (10 to be exact — I'm nice, I counted them for you!). But all we care about is
+    the last one, genes. Count your commas carefully!
+    */
+    function feedOnKitty(uint256 _zombieId, uint256 _kittyId) public {
+        uint256 kittyDna;
+        (, , , , , , , , , kittyDna) = kittyContract.getKitty(_kittyId);
+        feedAndMultiply(_zombieId, kittyDna, "kitty");
     }
 }
